2fda62ee2850263a384d9862f7da76afad9df3d8
thebadcc <thebadcc@outlook.com>
2021-08-02 16:34:01 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,3 @@
-L.control.attribution(false);
-var map = L.map('map', {
-    crs: L.CRS.Simple,
-	
-});
-
-
-var bounds = [[-500,500], [0,0]];
-var image = L.imageOverlay('https://github.com/thebadcc/thebad.cc/blob/main/images/world_map_v1.5.png?raw=true', bounds).addTo(map);
-
-var greenIcon = L.icon({
-    iconUrl: 'https://github.com/thebadcc/thebad.cc/blob/main/images/portalGIF_v1.1.gif?raw=true',
-    iconSize:     [50, 50], 
-    iconAnchor:   [0, 0], 
-    popupAnchor:  [25, 0]
-});
-
-L.marker([-655, -240], {icon: greenIcon}).addTo(map).bindPopup('<a target=_blank href = "https://www.cryptovoxels.com/play?coords=W@60E,458S,1.5U">16 Rand Extension</a>');
 
 (function($){
  $(function(){
@@ -227,12 +209,23 @@ async function getAccount() {
     legalLinks.style = "display: block !important";
     metaMaskRequired.style = "display: none !important";
 
+ var map = L.map('map', {
+    crs: L.CRS.Simple,
+});
+var bounds = [[0,0], [500,500]];
+var image = L.imageOverlay('https://github.com/thebadcc/thebad.cc/blob/main/images/world_map_v1.5.png?raw=true', bounds).addTo(map);
+
+var greenIcon = L.icon({
+    iconUrl: 'https://github.com/thebadcc/thebad.cc/blob/main/images/portalGIF_v1.1.gif?raw=true',
+    iconSize:     [50, 50], 
+    iconAnchor:   [0, 0], 
+    popupAnchor:  [25, 0]
+});
+
+L.marker([-655, -240], {icon: greenIcon}).addTo(map).bindPopup('<a target=_blank href = "https://www.cryptovoxels.com/play?coords=W@60E,458S,1.5U">16 Rand Extension</a>');
+
 map.fitBounds(bounds);
 map.setMaxBounds(bounds);
-map.on('drag', function() {
-    map.panInsideBounds(bounds, { animate: false });
-});
- map.invalidateSize();
 	
 }
 
